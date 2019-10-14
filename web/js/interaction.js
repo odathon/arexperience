1d9a11d12b11e9f33174602be85f10e143ce4f7b
odathon <56330678+odathon@users.noreply.github.com>
2019-10-15 02:49:52 +0800

    Add files via upload

--- web/js/interaction.js
+++ web/js/interaction.js
@@ -28,41 +28,98 @@ function hideSpeechBubbleIfNoMarker() {
 
 function handleClickEvent() {
   builders.forEach(function(builder) {
+    console.log(builder);
     var builderMarker = document.querySelector("#" + builder.name + "-marker");
+
+    var myParam = location.search.split('custid=')[1] ? location.search.split('custid=')[1] : '45466';
+    console.log(myParam);
+    var currentdate = new Date(); 
+    var obj=null;
+    var datetime =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+
+
+    if(builder.name==='pyra'){
+         obj ={
+      isbooth1: true,
+      timebooth: datetime
+  };
+} else if(builder.name==='frenchie'){
+         obj ={
+      isbooth3: true,
+      timebooth2: datetime
+  };
+}else if(builder.name==='toob'){
+       obj ={
+      isbooth5: true,
+      timebooth4: datetime
+  };
+}else if(builder.name==='lil'){
+        obj ={
+      isbooth4: true,
+      timebooth3: datetime
+  };
+}
+else if(builder.name==='hammer'){
+         obj ={
+      isbooth4: true,
+      timebooth3: datetime
+  };
+  }
+
+
     if (builderMarker && builderMarker.object3D.visible) {
       if (searchForBuilderTool(builder)){
-        toggleSpeechBubble(builder.successDialogue);
+        toggleSpeechBubble(builder,obj,myParam);
       } else {
-        toggleSpeechBubble(builder.dialogue,builder.name);
+        toggleSpeechBubble(builder,obj,myParam);
       }
     }
-  });
-
+ 
   tools.forEach(function(tool){
     var toolMarker = document.querySelector("#" + tool.name + "-marker");
     if (toolMarker && toolMarker.object3D.visible) {
-      toggleSpeechBubble(tool.dialogue,tool.name);
+     // toggleSpeechBubble(tool.dialogue,tool.name);
       if (!userState.hasBuilderTool(tool)) userState.addTool(tool);
     }
   });
+});
 }
 
-function toggleSpeechBubble(dialogue,name) {
+function toggleSpeechBubble(builder,obj,param) {
   var speechBubble = document.querySelector(".speech-bubble");
 	if (speechBubble.style.display === 'none' || !speechBubble.style.display) {
-    /* $.ajax({
-        url: " http://9083449f.ngrok.io/customers/customer/custid",
-        method: put
-    }).then(function(data) {
-       $('.${name}').append(data.id);
-       $('.greeting-content').append(data.content);
-    });*/
-		speechBubble.innerHTML = dialogue;
-		speechBubble.style.display = 'block';
-	} else {
-		speechBubble.style.display = 'none';
-	}
-};
+    speechBubble.innerHTML = builder.dialogue;
+    speechBubble.style.display = 'block';
+  } else {
+    speechBubble.style.display = 'none';
+  }
+
+  
+   //var uri="https://argamer.herokuapp.com/customers/customer/"+param
+  
+ /* console.log(obj);
+
+    $.ajax({ 
+     type:'post',
+     url:uri,
+     dataType :'jsonp',
+     data: obj,  //<------since your controller returns string use "text"
+     success: function(data) {
+    alert('Load was performed.');
+  }
+      
+		
+});
+*/
+
+
+}
 
 function searchForBuilderTool(builder) {
   return userState.tools.some(function(tool) {
--- web/js/models.js
+++ web/js/models.js
@@ -1,10 +1,12 @@
 var builders = [],
     tools = [];
 
-function ARModel(name, dialogue) {
+function ARModel(name, dialogue,id,time) {
     //we can make name link to the el id to find it on click?
     this.name = name;
     this.dialogue = dialogue;
+    this.id=id;
+    this.time=time;
     
 
 }
@@ -14,8 +16,8 @@ ARModel.prototype.speak = function() {
 }
 
 //Builder model
-function Builder(name, dialogue, tool, successDialogue) {
-    ARModel.call(this, name, dialogue);
+function Builder(name, dialogue, tool, successDialogue,id,time) {
+    ARModel.call(this, name, dialogue,id,time);
     this.tool = tool;
     this.successDialogue = successDialogue;
 }
@@ -31,46 +33,59 @@ Tool.prototype = Object.create(ARModel.prototype);
 
 function initiateModels() {
     var buildersArray = [
-      {
+      { 
         name: 'pyra',
-        dialogue: 'Hi there , I\'m Ora I am princess of Zorcona! I\'ve lost my sword. Let me know if you see it!',
+        dialogue: 'Hi there , I\'m Ora a HR Manager, My day to day job requires me to check employee wellness, time etc. With Oracle Cloud I can achieve that!',
         tool: new Tool('xyz', 'You have found Ora\'s Sword!'),
-        successDialogue: 'Thanks for my hammer!'
-      },
+        successDialogue: 'Thanks for my hammer!',
+        id: 'isbooth1',
+        time: 'timebooth' 
+      }, 
       {
         name: 'biggie',
         dialogue: 'Thank you helping me find my sword! Fortune favors te brave, be brave and look for an emerald Gem!',
         tool: new Tool('abc', 'You have found my gem!'),
-        successDialogue: 'My blocks have been found!'
+        successDialogue: 'My blocks have been found!',
+        id: 'isbooth2',
+        time: 'timebooth1'
       },
       {
         name: 'frenchie',
-        dialogue: 'Gem has special powers when combined with crown, Look for crown to unveil those powers. ',
+        dialogue: 'Gem of all Lands! Acquire me to get higher power to acquire and retain customers just like Oracle CX and Marketing Cloud.  ',
         tool: new Tool('qwer', 'You have found Frenchie\'s hat!'),
-        successDialogue: 'Merci!'
+        successDialogue: 'Merci!',
+        id: 'isbooth3',
+        time: 'timebooth2'
       },
       {
         name: 'lil',
-        dialogue: 'Just a step away from treasure.!!',
+        dialogue: 'As Sharp as a sword and precise should be your Database, let\'s drive Oracle Autonomous Database',
         tool: new Tool('klj', 'You have found Lil\'s screwdriver!'),
-        successDialogue: 'Thanks for the screwdriver!'
+        successDialogue: 'Thanks for the screwdriver!',
+        id: 'isbooth4',
+        time: 'timebooth3'
       },
       {
         name: 'toob',
-        dialogue: 'You have got superpowers! It\'s time for some  gold! ',
+        dialogue: 'A penny saved is a Penny Earned, with Oracle Financials Cloud it\'s reality ',
         tool: new Tool('nmo', 'You have finally Got the treasure'),
-        successDialogue: 'Thanks for finding my wrench!'
+        successDialogue: 'Thanks for finding my wrench!',
+        id: 'isbooth5',
+        time: 'timebooth4'
+
       },
       {
         name: 'hammer',
-        dialogue: 'You have finally got the treasure. To collect it kindly go to MCX booth!! ',
+        dialogue: 'The Crown of any industry, would be it\'s effective supply chain management. Experience it with Oracle SCM cloud.',
         tool: new Tool('nmo', 'You have finally Got the treasure'),
-        successDialogue: 'Thanks for finding my wrench!'
+        successDialogue: 'Thanks for finding my wrench!',
+        id:'booth6',
+        time: 'timebooth5'
       }
     ];
 
     buildersArray.forEach(function(builder){
-        builders.push(new Builder(builder.name, builder.dialogue, builder.tool, builder.successDialogue));
+        builders.push(new Builder(builder.name, builder.dialogue, builder.tool, builder.successDialogue, builder.id, builder.time));
         if (builder.tool) tools.push(builder.tool);
     });
 
